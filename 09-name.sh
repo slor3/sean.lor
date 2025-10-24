1dee9630f9118174694eddd3a02de716b621e8ca
slor3 <slor3@ggc.edu>
2025-10-24 15:17:18 -0400

    changed sites to bing, changed operation to ping (09-names.sh)

---
+++ 09-name.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+# look up ip addresses of various search engines
+
+servers="bing.com"
+
+for server in $servers; do
+    ping $server
+    echo "----------------------------"
+done
+
+# exercise: Change the list of servers and also the 
+# operation applied to them. For instance, use ping, 
+# traceroute, or nslookup with other options.
