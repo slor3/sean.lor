cbdbf394e649e9864b39c731a70623f1686f8773
slor3 <slor3@ggc.edu>
2025-10-24 15:25:06 -0400

    changed sites to safari, changed echo (09-name.sh)

---
+++ 09-name.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+# look up ip addresses of various search engines
+
+servers="safari.com"
+
+for server in $servers; do
+    nslookup $server
+    echo "----------"
+done
+
+# exercise: Change the list of servers and also the 
+# operation applied to them. For instance, use ping, 
+# traceroute, or nslookup with other options.
